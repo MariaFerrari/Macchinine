bb4bb5c5dcca9443a1a38b21562ce66fc76051ac
Maria Ferrari <maria.ferrari.studenti@isii.it>
2020-11-07 13:25:27 +0100

    Progetto macchinine V1, doppia MessageBox a fine gara, da revisionare

---
+++ MacchineApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MacchineApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker_R = new System.ComponentModel.BackgroundWorker();
+            this.pbx_strada = new System.Windows.Forms.PictureBox();
+            this.pbx_red = new System.Windows.Forms.PictureBox();
+            this.pbx_black = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker_N = new System.ComponentModel.BackgroundWorker();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_rivincita = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_strada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_black)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker_R
+            // 
+            this.backgroundWorker_R.WorkerReportsProgress = true;
+            this.backgroundWorker_R.WorkerSupportsCancellation = true;
+            this.backgroundWorker_R.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker_R.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker_R.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // pbx_strada
+            // 
+            this.pbx_strada.Image = global::MacchineApp1.Properties.Resources.road_trip_background_10864211;
+            this.pbx_strada.Location = new System.Drawing.Point(0, -1);
+            this.pbx_strada.Name = "pbx_strada";
+            this.pbx_strada.Size = new System.Drawing.Size(1212, 453);
+            this.pbx_strada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_strada.TabIndex = 0;
+            this.pbx_strada.TabStop = false;
+            this.pbx_strada.Click += new System.EventHandler(this.pbx_strada_Click);
+            // 
+            // pbx_red
+            // 
+            this.pbx_red.BackColor = System.Drawing.Color.Black;
+            this.pbx_red.Image = global::MacchineApp1.Properties.Resources.macchinarossa;
+            this.pbx_red.Location = new System.Drawing.Point(0, 61);
+            this.pbx_red.Name = "pbx_red";
+            this.pbx_red.Size = new System.Drawing.Size(240, 114);
+            this.pbx_red.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_red.TabIndex = 1;
+            this.pbx_red.TabStop = false;
+            this.pbx_red.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pbx_black
+            // 
+            this.pbx_black.BackColor = System.Drawing.Color.Black;
+            this.pbx_black.Image = global::MacchineApp1.Properties.Resources.macchinabianca;
+            this.pbx_black.Location = new System.Drawing.Point(0, 271);
+            this.pbx_black.Name = "pbx_black";
+            this.pbx_black.Size = new System.Drawing.Size(240, 114);
+            this.pbx_black.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_black.TabIndex = 2;
+            this.pbx_black.TabStop = false;
+            // 
+            // backgroundWorker_N
+            // 
+            this.backgroundWorker_N.WorkerReportsProgress = true;
+            this.backgroundWorker_N.WorkerSupportsCancellation = true;
+            this.backgroundWorker_N.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker_N.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker_N.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(1227, 92);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "Inizio";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(1227, 169);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 4;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_rivincita
+            // 
+            this.btn_rivincita.Location = new System.Drawing.Point(1227, 247);
+            this.btn_rivincita.Name = "btn_rivincita";
+            this.btn_rivincita.Size = new System.Drawing.Size(75, 23);
+            this.btn_rivincita.TabIndex = 5;
+            this.btn_rivincita.Text = "Rivincita";
+            this.btn_rivincita.UseVisualStyleBackColor = true;
+            this.btn_rivincita.Click += new System.EventHandler(this.btn_rivincita_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 450);
+            this.Controls.Add(this.btn_rivincita);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.pbx_black);
+            this.Controls.Add(this.pbx_red);
+            this.Controls.Add(this.pbx_strada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_strada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_black)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker_R;
+        private System.Windows.Forms.PictureBox pbx_strada;
+        private System.Windows.Forms.PictureBox pbx_red;
+        private System.Windows.Forms.PictureBox pbx_black;
+        private System.ComponentModel.BackgroundWorker backgroundWorker_N;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_rivincita;
+    }
+}
+
---
+++ MacchineApp1/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MacchineApp1
+{
+    public partial class Form1 : Form
+    {
+        #region controlli erroneamente dichiarati
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pbx_strada_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+        public int Traguardo //lunghezza della strada
+        {
+            get { return pbx_strada.Width; }
+        }
+        Automobile auto_rossa;
+        Automobile auto_nera;
+        public Form1()
+        {
+            InitializeComponent();
+            //inizializzazione auto
+            auto_rossa = new Automobile(pbx_red, "ROSSA", Traguardo);
+            auto_nera = new Automobile(pbx_black, "NERA", Traguardo);
+        }
+        private bool Vittoria() 
+        {
+
+            return auto_rossa.Vittoria || auto_nera.Vittoria;
+        }
+        
+        #region bgw
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = (BackgroundWorker)sender;
+            Automobile auto = (Automobile)e.Argument;
+            Random rnd = new Random();
+            //finché una delle due auto non vince
+            while (!Vittoria())
+            {
+                if (bgw.CancellationPending)
+                {
+                    //se viene premuto il pulsante stop
+                    e.Result = false;
+                    return;
+                }
+                else
+                {
+                    int spostamento = rnd.Next(1, 11);
+                    //muovo l'auto
+                    auto.Posizione += spostamento;
+                    //mando al progress changed la posizione che l'auto deve avere
+                    bgw.ReportProgress(auto.Posizione, auto);
+                    Thread.Sleep(100);
+                }
+            }
+
+            //un'auto è arrivata senza che il pulsante stop sia stato premuto
+            if (bgw.CancellationPending)
+            {
+                //se viene premuto il pulsante stop
+                e.Result = false;
+                return;
+            }
+            e.Result = true;
+
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Automobile auto = (Automobile)e.UserState;
+            if (e.ProgressPercentage != Traguardo - auto.Lunghezza) //l'auto non arriva alla fine
+            {
+                auto.Muovi();
+                Refresh();
+            }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btn_rivincita.Show();
+            var cancellato=!(bool)e.Result; //e.Result è falso se viene cancellato quindi lo nego
+            if (cancellato)
+            {
+                //se è stato cancellato riporto le auto a 0
+                auto_rossa.Posizione = 0;
+                auto_nera.Posizione = 0;
+                Refresh();
+            }
+            else 
+            {
+                //altrimenti mostro le messagebox
+                if (auto_rossa.Vittoria && auto_nera.Vittoria)
+                    MessageBox.Show("PAREGGIO");
+                else
+                if (auto_rossa.Vittoria)
+                    MessageBox.Show("ROSSA HA VINTO");
+                else MessageBox.Show("NERA HA VINTO");
+                btn_start.Hide();
+            }
+        }
+        #endregion
+
+        private void btn_rivincita_Click(object sender, EventArgs e)
+        {
+            auto_rossa.Posizione = 0;
+            auto_nera.Posizione = 0;
+            Refresh();
+            btn_start_Click(sender, e);
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            btn_rivincita.Hide();
+            btn_start.Hide();
+            backgroundWorker_N.RunWorkerAsync(auto_nera);
+            backgroundWorker_R.RunWorkerAsync(auto_rossa);
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            btn_rivincita.Show();
+            btn_start.Show();
+            backgroundWorker_N.CancelAsync();
+            backgroundWorker_R.CancelAsync();
+        }
+
+        private void btn_pos_Click(object sender, EventArgs e)
+        {
+            auto_rossa.Posizione = 0;
+            auto_nera.Posizione = 0;
+            Refresh();
+        }
+    }
+    public class Automobile
+    {
+        int _fine;
+        PictureBox _pb;
+        Point _pos;
+        public string Nome { get; private set; }
+        public Automobile(PictureBox pb, string nome, int l_tracciato)
+        {
+            _pb = pb;
+            _fine = l_tracciato;
+        }
+        public int Posizione
+        {
+            get { return _pos.X; }
+            set
+            {
+                _pos = new Point(value, _pb.Location.Y);
+            }
+        }
+        public int Lunghezza
+        {
+            get { return _pb.Width; }
+        }
+        public bool Vittoria
+        {
+            get { return _pos.X >= _fine-_pb.Width ? true : false; }
+        }
+        
+        private Point Pos
+        {
+            get { return new Point(Posizione, _pb.Location.Y); }
+        }
+        public void Muovi()
+        {
+            _pb.Location = Pos;
+        }
+        //public override bool Equals(object obj)
+        //{
+        //    Automobile auto = obj as Automobile;
+        //    if (auto == null) return false;
+        //    return auto.Nome == this.Nome;
+        //}
+    }
+}
