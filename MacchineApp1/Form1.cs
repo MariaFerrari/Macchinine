fd9d36f3c9d6f5df40e50e008afe6baae804b643
Maria Ferrari <maria.ferrari.studenti@isii.it>
2020-11-08 13:39:35 +0100

    V2, MessageBox non doppie, pareggio gestito

--- MacchineApp1/Form1.cs
+++ MacchineApp1/Form1.cs
@@ -29,6 +29,8 @@ namespace MacchineApp1
         }
         Automobile auto_rossa;
         Automobile auto_nera;
+        Random rnd = new Random();
+
         public Form1()
         {
             InitializeComponent();
@@ -36,25 +38,19 @@ namespace MacchineApp1
             auto_rossa = new Automobile(pbx_red, "ROSSA", Traguardo);
             auto_nera = new Automobile(pbx_black, "NERA", Traguardo);
         }
-        private bool Vittoria() 
-        {
-
-            return auto_rossa.Vittoria || auto_nera.Vittoria;
-        }
         
         #region bgw
         private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker bgw = (BackgroundWorker)sender;
             Automobile auto = (Automobile)e.Argument;
-            Random rnd = new Random();
             //finché una delle due auto non vince
-            while (!Vittoria())
+            while (!(auto_rossa.Vittoria || auto_nera.Vittoria))
             {
                 if (bgw.CancellationPending)
                 {
                     //se viene premuto il pulsante stop
-                    e.Result = false;
+                    e.Result = (false, auto);
                     return;
                 }
                 else
@@ -64,59 +60,78 @@ namespace MacchineApp1
                     auto.Posizione += spostamento;
                     //mando al progress changed la posizione che l'auto deve avere
                     bgw.ReportProgress(auto.Posizione, auto);
-                    Thread.Sleep(100);
+                    Thread.Sleep(50);
                 }
             }
+            if (auto_rossa.Vittoria && auto_nera.Vittoria)
+            {
+                //se le auto pareggiano cancello uno dei due bgw
+                backgroundWorker_N.CancelAsync();
+            }
 
-            //un'auto è arrivata senza che il pulsante stop sia stato premuto
+            //il bgw cancellato
             if (bgw.CancellationPending)
             {
-                //se viene premuto il pulsante stop
-                e.Result = false;
+                //chiama il pareggio
+                Pareggio();
+                //e ritorna falso
+                e.Result = (false, auto);
                 return;
             }
-            e.Result = true;
 
+            //se non ci sono pareggi o cancellazioni, ritorno vero
+            e.Result = (true, auto);
         }
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             Automobile auto = (Automobile)e.UserState;
-            if (e.ProgressPercentage != Traguardo - auto.Lunghezza) //l'auto non arriva alla fine
+            //finché viene chiamato il progress changed (ovvero finché si è nel while)
+            if (e.ProgressPercentage != -1) 
             {
+                //muovo l'auto
                 auto.Muovi();
-                Refresh();
             }
         }
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            //quando ho finito il dowork
             btn_rivincita.Show();
-            var cancellato=!(bool)e.Result; //e.Result è falso se viene cancellato quindi lo nego
-            if (cancellato)
+            var (non_soppresso, auto) = (ValueTuple<bool, Automobile>)e.Result;
+            //se il processo non è stato cancellato
+            if (non_soppresso) 
+            { 
+                //e se l'auto ha vinto 
+                if (auto.Vittoria)
+                    //stampo che ha vinto
+                    MessageBox.Show(auto.Nome + " HA VINTO");
+                btn_start.Hide();
+                btn_stop.Hide();
+            }
+            else
             {
                 //se è stato cancellato riporto le auto a 0
                 auto_rossa.Posizione = 0;
                 auto_nera.Posizione = 0;
-                Refresh();
-            }
-            else 
-            {
-                //altrimenti mostro le messagebox
-                if (auto_rossa.Vittoria && auto_nera.Vittoria)
-                    MessageBox.Show("PAREGGIO");
-                else
-                if (auto_rossa.Vittoria)
-                    MessageBox.Show("ROSSA HA VINTO");
-                else MessageBox.Show("NERA HA VINTO");
-                btn_start.Hide();
+                auto_nera.Muovi();
+                auto_rossa.Muovi();
             }
         }
         #endregion
+
+        #region pulsanti e metodi
+        private void Pareggio()
+        {
+            auto_rossa.Posizione = 0;
+            auto_nera.Posizione = 0;
+            MessageBox.Show("PAREGGIO");
+        }
 
         private void btn_rivincita_Click(object sender, EventArgs e)
         {
             auto_rossa.Posizione = 0;
             auto_nera.Posizione = 0;
-            Refresh();
+            auto_nera.Muovi();
+            auto_rossa.Muovi();
             btn_start_Click(sender, e);
         }
 
@@ -124,6 +139,7 @@ namespace MacchineApp1
         {
             btn_rivincita.Hide();
             btn_start.Hide();
+            btn_stop.Show();
             backgroundWorker_N.RunWorkerAsync(auto_nera);
             backgroundWorker_R.RunWorkerAsync(auto_rossa);
         }
@@ -135,26 +151,21 @@ namespace MacchineApp1
             backgroundWorker_N.CancelAsync();
             backgroundWorker_R.CancelAsync();
         }
-
-        private void btn_pos_Click(object sender, EventArgs e)
-        {
-            auto_rossa.Posizione = 0;
-            auto_nera.Posizione = 0;
-            Refresh();
-        }
+        #endregion
     }
     public class Automobile
     {
-        int _fine;
-        PictureBox _pb;
+        int lunghezza_tracciato;
+        PictureBox _pb; 
         Point _pos;
         public string Nome { get; private set; }
         public Automobile(PictureBox pb, string nome, int l_tracciato)
         {
             _pb = pb;
-            _fine = l_tracciato;
+            lunghezza_tracciato = l_tracciato;
+            Nome = nome;
         }
-        public int Posizione
+        public int Posizione //posizione dell'auto
         {
             get { return _pos.X; }
             set
@@ -162,28 +173,23 @@ namespace MacchineApp1
                 _pos = new Point(value, _pb.Location.Y);
             }
         }
-        public int Lunghezza
+        public int Lunghezza //lunghezza orizziontale dell'auto
         {
             get { return _pb.Width; }
         }
-        public bool Vittoria
+        public bool Vittoria 
         {
-            get { return _pos.X >= _fine-_pb.Width ? true : false; }
+            get { return _pos.X >= lunghezza_tracciato - _pb.Width ? true : false; }
         }
-        
-        private Point Pos
+
+        private Point Pos //punto per settare la posizione della pb (è privato e usato solo nel metodo muovi)
         {
             get { return new Point(Posizione, _pb.Location.Y); }
         }
         public void Muovi()
         {
+            //assegna alla pb la sua effettiva posizione
             _pb.Location = Pos;
         }
-        //public override bool Equals(object obj)
-        //{
-        //    Automobile auto = obj as Automobile;
-        //    if (auto == null) return false;
-        //    return auto.Nome == this.Nome;
-        //}
     }
 }
